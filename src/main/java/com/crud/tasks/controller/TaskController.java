a770600059991d90016c530c1d5fa6a91ec958a7
SzymBurz <szb210@gmail.com>
2022-07-19 17:12:37 +0200

    wyszukiwanie pojedynczego zadania

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,30 +1,37 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
 public class TaskController {
 
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
     @RequestMapping(method = RequestMethod.GET, value = "")
+    @GetMapping
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "")
-    public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+    @RequestMapping(method = RequestMethod.GET, value = "{taskId}")
+    public Task getTask(@PathVariable Long taskId) {
+        return service.getTaskById(taskId);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "")
-    public void deleteTask(Long taskId) {
+    @RequestMapping(method = RequestMethod.DELETE, value = "{taskId}")
+    public void deleteTask(@PathVariable Long taskId) {
 
     }
 
